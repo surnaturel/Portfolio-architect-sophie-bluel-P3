cd386eea47db1010b57c730aec67028c6c1b24df
surnaturel <numirix.yannick@gmail.com>
2023-06-28 23:53:18 +0000

    prendre les projets dynamiquement sur l'api

---
+++ FrontEnd/projet.js
@@ -0,0 +1,47 @@
+let monProjets = window.localStorage.getItem('monProjets');
+
+
+if(monProjets === null){
+    let reponse = await fetch('http://localhost:5678/api/works')
+    monProjets = await reponse.json()
+    let valeurProjets = JSON.stringify(monProjets);
+    window.localStorage.setItem("monProjets", valeurProjets);
+
+}else{
+    monProjets = JSON.parse(monProjets)
+}
+
+function afficheProjets(monProjets){
+    monProjets.forEach(projet => {
+        let gallery  = document.querySelector('.gallery')
+        let elementProjet = document.createElement('figure')
+        let elementImage = document.createElement('img')
+        elementImage.src = projet.imageUrl
+        let elementTitre = document.createElement('p')
+        elementTitre.innerText = projet.title
+
+        gallery.appendChild(elementProjet)
+        elementProjet.appendChild(elementImage)
+        elementProjet.appendChild(elementTitre)
+
+        console.log(projet)
+    });
+
+}
+
+
+afficheProjets(monProjets)
+
+function filtreBar(){
+    let tousBtn = document.getElementById('tous')
+    let objetBtn = document.getElementById('objets')
+    let appartementBtn = document.getElementById('appartement')
+    let hotelRestaurantBtn = document.getElementById('hotelRestauration')
+    let buttonBtns = document.querySelectorAll("#portfolio .filtreBar button")
+
+    
+    console.log(buttonBtns)
+
+}
+
+filtreBar()
