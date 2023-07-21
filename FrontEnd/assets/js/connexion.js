8ce4cb3dcc8160cb8de96767ff45157473a3cb04
surnaturel <numirix.yannick@gmail.com>
2023-07-21 15:49:14 +0000

    travailler la page LOGIN

--- FrontEnd/assets/js/connexion.js
+++ FrontEnd/assets/js/connexion.js
@@ -7,17 +7,23 @@ form.addEventListener("submit", async (event) => {
     const formData = new FormData(form);
     let email = formData.get('email'); // Récupérer la valeur de l'email
     let motDePasse = formData.get('motPasse');
+    console.log(email)
     console.log(formData)
-
+    let data = {
+        email: email,
+        password: motDePasse
+      }
+      console.log(data)
     try {
         const response = await fetch('http://localhost:5678/api/users/login', {
             method: "POST",
-            body: formData,
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(data)
         });
 
         if (response.ok) {
             // La requête a réussi, rediriger vers la page de succès ou faire autre chose
-            window.location.href = 'index.html'
+            window.location.href = 'modifProjet.html'
         } else {
             // La requête a échoué, afficher un message d'erreur
             errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
