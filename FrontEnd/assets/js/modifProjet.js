4d977f16d367d56b5c143cf42d9f89c99afd5f30
surnaturel <numirix.yannick@gmail.com>
2023-07-24 17:59:03 +0000

    faire le modal

---
+++ FrontEnd/assets/js/modifProjet.js
@@ -0,0 +1,55 @@
+let monProjets = window.localStorage.getItem('monProjets');
+let mesBoutons = window.localStorage.getItem('mesBoutons')
+
+if(monProjets === null){
+    let reponse = await fetch('http://localhost:5678/api/works')
+    monProjets = await reponse.json()
+    let valeurProjets = JSON.stringify(monProjets);
+    window.localStorage.setItem("monProjets", valeurProjets);
+
+}else{
+    monProjets = JSON.parse(monProjets)
+}
+
+function afficheProjets(monProjets){
+    let gallery  = document.querySelector('.gallery')
+    gallery.innerHTML = " "
+    monProjets.forEach(projet => {
+
+        let elementProjet = document.createElement('figure')
+        let elementImage = document.createElement('img')
+        elementImage.src = projet.imageUrl
+        let elementTitre = document.createElement('p')
+        elementTitre.innerText = projet.title
+
+        gallery.appendChild(elementProjet)
+        elementProjet.appendChild(elementImage)
+        elementProjet.appendChild(elementTitre)
+
+    });
+
+}
+
+afficheProjets(monProjets)
+
+let modifGalery = document.getElementById('modifGalery')
+modifGalery.addEventListener('click', function(event) {
+    event.preventDefault()
+    console.log(monProjets)
+    let modal = document.getElementById('galery-modal')
+    modal.innerHTML = " "
+    monProjets.forEach(projet => {
+
+        let elementModal = document.createElement('figure')
+        let modalImage = document.createElement('img')
+        modalImage.src = projet.imageUrl
+        let modifTitre = document.createElement('a')
+        modifTitre.innerText = 'editer'
+
+        modal.appendChild(elementModal)
+        elementModal.appendChild(modalImage)
+        elementModal.appendChild(modifTitre)
+
+    });
+
+})
