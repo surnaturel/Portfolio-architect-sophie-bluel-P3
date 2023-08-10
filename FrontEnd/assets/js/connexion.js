2164e9939c014c1e6edc37bba6b7563b67cf7765
surnaturel <numirix.yannick@gmail.com>
2023-08-10 14:18:44 +0000

    terminer le modal a 60%

--- FrontEnd/assets/js/connexion.js
+++ FrontEnd/assets/js/connexion.js
@@ -1,47 +1,46 @@
-import { affichageModal } from 'index.js' ;
-
-
-const form = document.getElementById("formulaire");
-const errorMessage = document.getElementById("err");
-
-form.addEventListener("submit", async (event) => {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    // Mettez ici le code commun que vous souhaitez partager
+    const form = document.getElementById("formulaire");
+    const errorMessage = document.getElementById("err");
     
-    const email = form.email.value;
-    const motDePasse = form.motPasse.value;
-    let data = {
-        email: email,
-        password: motDePasse
-      }
-      console.log(data)
-    try {
-        const response = await fetch('http://localhost:5678/api/users/login', {
-            method: "POST",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(data)
-        });
-
-        if (response.ok) {
-            // La requête a réussi, rediriger vers la page de succès ou faire autre chose
-            console.log(response.ok)
-            console.log(response)
-            const responseData = await response.json();
-            const token = responseData.token;
-            // Stocker le token dans le local storage
-            if(token){
-                window.localStorage.setItem("tokenLogin", token);
-                console.log("Token récupéré :", token);
-                affichageModal()
-            }
-            
-            
-            
-        } else {
-            // La requête a échoué, afficher un message d'erreur
-            errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const email = form.email.value;
+        const motDePasse = form.motPasse.value;
+        let data = {
+            email: email,
+            password: motDePasse
         }
-    } catch (error) {
-        // Une erreur s'est produite lors de la requête, afficher un message d'erreur
-        errorMessage.textContent = "Une erreur s'est produite, veuillez réessayer plus tard.";
-    }
+          console.log(data)
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login', {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify(data)
+            });
+    
+            if (response.ok) {
+                // La requête a réussi, rediriger vers la page de succès ou faire autre chose
+                console.log(response.ok)
+                console.log(response)
+                const responseData = await response.json();
+                const token = responseData.token;
+                // Stocker le token dans le local storage
+                if (token) {
+                    window.localStorage.setItem("tokenLogin", token);
+                    console.log("Token récupéré :", token);
+                    window.localStorage.setItem("isConnected", "true"); // Indicateur de connexion réussie
+                    window.location.href = 'index.html';
+                }
+            } else {
+                // La requête a échoué, afficher un message d'erreur
+                errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
+            }
+        } catch (error) {
+            // Une erreur s'est produite lors de la requête, afficher un message d'erreur
+            errorMessage.textContent = "Une erreur s'est produite, veuillez réessayer plus tard.";
+        }
+    });
 });
+
+
--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,123 +1,170 @@
-let monProjets = window.localStorage.getItem('monProjets');
-let mesBoutons = window.localStorage.getItem('mesBoutons')
+//import { afficheBoutonsModif } from "./index.js";
+document.addEventListener("DOMContentLoaded", async function() {
+    const isConnected = window.localStorage.getItem("isConnected");
+    console.log(isConnected)
+    if (isConnected === "true") {
+        //afficheBoutonsModif();
+        // Faites d'autres actions nécessaires pour l'utilisateur connecté
+        afficheBoutonsModif1()
+        afficheBoutonsModif2()
+    }
 
-if(monProjets === null){
-    let reponse = await fetch('http://localhost:5678/api/works')
-    monProjets = await reponse.json()
-    let valeurProjets = JSON.stringify(monProjets);
-    window.localStorage.setItem("monProjets", valeurProjets);
+    // Reste du code pour index.html...
+    let monProjets = window.localStorage.getItem('monProjets');
+    let mesBoutons = window.localStorage.getItem('mesBoutons')
 
-}else{
-    monProjets = JSON.parse(monProjets)
-}
+    if(monProjets === null){
+        let reponse = await fetch('http://localhost:5678/api/works')
+        monProjets = await reponse.json()
+        let valeurProjets = JSON.stringify(monProjets);
+        window.localStorage.setItem("monProjets", valeurProjets);
 
+    }else{
+        monProjets = JSON.parse(monProjets)
+    }
 
-if(mesBoutons === null){
-    let reponseBtn = await fetch('http://localhost:5678/api/categories')
-    mesBoutons = await reponseBtn.json()
-    let valeurButons = JSON.stringify(mesBoutons);
-    window.localStorage.setItem("mesBoutons", valeurButons);
+    if(mesBoutons === null){
+        let reponseBtn = await fetch('http://localhost:5678/api/categories')
+        mesBoutons = await reponseBtn.json()
+        let valeurButons = JSON.stringify(mesBoutons);
+        window.localStorage.setItem("mesBoutons", valeurButons);
 
-}else{
-    mesBoutons = JSON.parse(mesBoutons)
-}
+    }else{
+        mesBoutons = JSON.parse(mesBoutons)
+    }
 
-console.log(mesBoutons)
-
-function afficheProjets(monProjets){
-    let gallery  = document.querySelector('.gallery')
-    gallery.innerHTML = " "
-    monProjets.forEach(projet => {
-
-        let elementProjet = document.createElement('figure')
-        let elementImage = document.createElement('img')
-        elementImage.src = projet.imageUrl
-        let elementTitre = document.createElement('p')
-        elementTitre.innerText = projet.title
-
-        gallery.appendChild(elementProjet)
-        elementProjet.appendChild(elementImage)
-        elementProjet.appendChild(elementTitre)
-
-    });
-
-}
-afficheProjets(monProjets)
-function afficheBoutons(mesBoutons){
-    let boutonFiltre = document.querySelector('.filtreBar')
-    mesBoutons.forEach(btnFiltre  => {
-        let btn = document.createElement('button')
-        btn.id = btnFiltre.name
-        btn.innerText = btnFiltre.name
-        btn.type = 'submit'
-        btn.style.cursor = 'pointer'
-        boutonFiltre.appendChild(btn)
-    })
-
-}
-afficheBoutons(mesBoutons)
-
-function filtreBar(){
-    let buttonBtns = document.querySelectorAll("#portfolio .filtreBar button")
-    buttonBtns.forEach(bouton => {
-        bouton.addEventListener('click', function(event) {
-            let id = event.target.id
-            if(id === 'Tous'){
-                afficheProjets(monProjets)
-                console.log(monProjets)
-            }else{
-                let listeProjet = monProjets.filter(listeP  => listeP.category.name === id)
-                afficheProjets(listeProjet)
-                console.log(listeProjet)
-                console.log(id)
-            }
-        })
-    });
-    
-}
-
-filtreBar()
-
-
-
-function affichageModal(){
-    let modifGalery = document.getElementById('modifGalery')
-    modifGalery.addEventListener('click', function(event) {
-        event.preventDefault()
-        console.log(monProjets)
-        let modal = document.getElementById('galery-modal')
-        let modale = document.getElementById('modale')
-        let body = document.getElementsByName('body')
-        modale.style.display = 'flex'
-        modal.innerHTML = " "
+    console.log(mesBoutons)
+    function afficheProjets(monProjets){
+        let gallery  = document.querySelector('.gallery')
+        console.log(gallery)
+        gallery.innerHTML = " "
         monProjets.forEach(projet => {
 
-            let elementModal = document.createElement('figure')
-            let modalImage = document.createElement('img')
-            modalImage.src = projet.imageUrl
-            let modifTitre = document.createElement('a')
-            modifTitre.innerText = 'editer'
+            let elementProjet = document.createElement('figure')
+            let elementImage = document.createElement('img')
+            elementImage.src = projet.imageUrl
+            let elementTitre = document.createElement('p')
+            elementTitre.innerText = projet.title
 
-            modal.appendChild(elementModal)
-            elementModal.appendChild(modalImage)
-            elementModal.appendChild(modifTitre)
+            gallery.appendChild(elementProjet)
+            elementProjet.appendChild(elementImage)
+            elementProjet.appendChild(elementTitre)
 
         });
-    })
-}
 
-function afficheBoutonsModif(){
-    let btnModif = document.createElement('div')
-    btnModif.id = 'modif'
-    let btnIcnone = document.createElement('i')
-    btnIcnone.className = 'fa-regular fa-pen-to-square'
-    let btnLien = document.createElement('a')
-    btnLien.innerText = 'Modifier'
+    }
+    afficheProjets(monProjets)
+    function afficheBoutons(mesBoutons){
+        let boutonFiltre = document.querySelector('.filtreBar')
+        mesBoutons.forEach(btnFiltre  => {
+            let btn = document.createElement('button')
+            btn.id = btnFiltre.name
+            btn.innerText = btnFiltre.name
+            btn.type = 'submit'
+            btn.style.cursor = 'pointer'
+            boutonFiltre.appendChild(btn)
+        })
 
-    const sectionIntroduction = document.getElementById('introduction');
-    const figureElement = sectionIntroduction.querySelector('figure');
+    }
+    afficheBoutons(mesBoutons)
 
-    figureElement.appendChild(btnModif)
-    btnModif.appendChild(btnIcnone)
-    btnModif.appendChild(btnLien)
-}
+    function filtreBar(){
+        let buttonBtns = document.querySelectorAll("#portfolio .filtreBar button")
+        buttonBtns.forEach(bouton => {
+            bouton.addEventListener('click', function(event) {
+                let id = event.target.id
+                if(id === 'Tous'){
+                    afficheProjets(monProjets)
+                    console.log(monProjets)
+                }else{
+                    let listeProjet = monProjets.filter(listeP  => listeP.category.name === id)
+                    afficheProjets(listeProjet)
+                    console.log(listeProjet)
+                    console.log(id)
+                }
+            })
+        });
+        
+    }
+
+    filtreBar()
+    
+    function afficheBoutonsModif1(){
+        let filtreBarSupri = document.querySelector('.filtreBar');
+        filtreBarSupri.style.display = 'none';
+
+        var btnModif1 = document.createElement('div')
+        btnModif1.classList.add("modif")
+        var btnIcnone = document.createElement('i')
+        btnIcnone.classList.add("fa-regular", "fa-pen-to-square");
+        var btnLien = document.createElement('a')
+        btnLien.innerText = 'Modifier'
+        console.log('je suis dans la function')
+    
+        const sectionIntroduction = document.getElementById('introduction');
+        const figureElement = sectionIntroduction.querySelector('figure');
+            
+        figureElement.appendChild(btnModif1)
+        btnModif1.appendChild(btnIcnone)
+        btnModif1.appendChild(btnLien)
+        console.log(btnModif1)
+    }
+
+    function afficheBoutonsModif2(){
+        let filtreBarSupri = document.querySelector('.filtreBar');
+        filtreBarSupri.style.display = 'none';
+
+        var btnModif = document.createElement('div')
+        btnModif.classList.add("modif")
+        var btnIcnone = document.createElement('i')
+        btnIcnone.classList.add("fa-regular", "fa-pen-to-square");
+        var btnLien = document.createElement('a')
+        btnLien.innerText = 'Modifier'
+        btnIcnone.style.id = 'modifGalery'
+        console.log('je suis dans la function')
+        
+        const sectionportfolio = document.getElementById('portfolio');
+        const aticleElement = sectionportfolio.querySelector('article');
+
+        aticleElement.appendChild(btnModif)
+        btnModif.appendChild(btnIcnone)
+        btnModif.appendChild(btnLien)
+    }
+
+    function affichageModal(){
+        let modifGalery = document.getElementById('modifGalery')
+        modifGalery.addEventListener('click', function(event) {
+            event.preventDefault()
+            console.log(monProjets)
+            let modal = document.getElementById('galery-modal')
+            let modale = document.getElementById('modale')
+            const bodyBeforeElement = document.createElement('div');
+    
+            // JavaScript
+            document.addEventListener("DOMContentLoaded", function() {
+                const openButton = document.getElementById('openButton'); // Remplacez 'openButton' par l'ID de votre bouton
+    
+                openButton.addEventListener('click', function() {
+                    document.body.classList.add('open-overlay');
+                });
+            });
+    
+            modale.style.display = 'flex'
+            modal.innerHTML = " "
+            monProjets.forEach(projet => {
+                let elementModal = document.createElement('figure')
+                let modalImage = document.createElement('img')
+                modalImage.src = projet.imageUrl
+                let modifTitre = document.createElement('a')
+                modifTitre.innerText = 'editer'
+    
+                modal.appendChild(elementModal)
+                elementModal.appendChild(modalImage)
+                elementModal.appendChild(modifTitre)
+    
+            });
+        })
+    }
+    affichageModal()
+
+});
